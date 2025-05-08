991f635d454b74fc9c015720393e5040e2c2a0fe
mzvast <mzvast@gmail.com>
2025-05-08 17:48:45 +0800

    css for navbar

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -156,6 +156,12 @@ const config: Config = {
   ],
 
   themeConfig: {
+    algolia: {
+      // todo: test only, to be replaced
+      appId: 'X1Z85QJPUV',
+      apiKey: 'bf7211c161e8205da2f933a02534105a',
+      indexName: 'docusaurus-2',
+    },
     liveCodeBlock: {
       /**
        * The position of the live playground, above or under the editor
