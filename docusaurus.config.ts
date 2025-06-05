6875a5691f91428b688cabb449f17aac97eee2b0
mzvast <mzvast@gmail.com>
2025-06-05 14:45:11 +0800

    change fonts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,18 +10,38 @@ const PROJ_NAME = process.env.PROJECT_NAME || 'my-default-project';
 
 const config: Config = {
   headTags: [
+    // {
+    //   tagName: 'link',
+    //   attributes: {
+    //     rel: 'preconnect',
+    //     href: '//rsms.me/',
+    //   },
+    // },
     {
       tagName: 'link',
       attributes: {
         rel: 'preconnect',
-        href: '//rsms.me/',
+        href: '//fonts.googleapis.com',
+      },
+    },
+    {
+      tagName: 'link',
+      attributes: {
+        rel: 'preconnect',
+        href: '//fonts.gstatic.com',
+        crossorigin: 'true',
       },
     },
   ],
 
   stylesheets: [
+    // {
+    //   href: '//rsms.me/inter/inter.css',
+    //   type: 'text/css',
+    // },
+
     {
-      href: '//rsms.me/inter/inter.css',
+      href: '//fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap',
       type: 'text/css',
     },
   ],
