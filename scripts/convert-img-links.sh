fda5d6a6d4b865f414ec2eea8ab79994baa80810
mzvast <mzvast@gmail.com>
2025-06-10 13:35:11 +0800

    update script for img link replacement

--- scripts/convert-img-links.sh
+++ scripts/convert-img-links.sh
@@ -16,8 +16,8 @@ else
 	SED_INPLACE=(-i)
 fi
 
-# 用 sed 替换 <a><img></a> 为 ![alt](src)，不创建备份
+# 用 sed 替换 <a><img></a> 为 <Image img={require("src")} alt="alt" />
 sed "${SED_INPLACE[@]}" -E \
-	's#<a[^>]*><img[^>]*src="([^"]+)"[^>]*alt="([^"]+)"[^>]*/?></a>#![\2](\1)#g' "$input_file"
+  's#<a[^>]*><img[^>]*src="([^"]+)"[^>]*alt="([^"]+)"[^>]*/?></a>#<Image img={require("\1")} alt="\2" />#g' "$input_file"
 
 echo "✅ 已就地替换完成：$input_file"
