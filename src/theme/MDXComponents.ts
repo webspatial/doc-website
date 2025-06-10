77aaecec7708a0c29a28551945b9cbe746bbccb3
mzvast <mzvast@gmail.com>
2025-06-10 13:33:53 +0800

    register mdx image comp

---
+++ src/theme/MDXComponents.ts
@@ -0,0 +1,9 @@
+import React from 'react';
+// Import the original mapper
+import MDXComponents from '@theme-original/MDXComponents';
+import Image from '@theme/IdealImage';
+export default {
+  // Re-use the default mapping
+  ...MDXComponents,
+  Image
+};
