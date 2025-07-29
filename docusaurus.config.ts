dc55c0c87f4523016f0278b4cb6d76104755b98c
wangyang <wangyang.dev@bytedance.com>
2025-07-29 14:01:47 +0800

    optimize css load

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -59,6 +59,20 @@ const config: Config = {
         gtag('config', '${process.env.GA_ID}');
       `,
     },
+    {
+      tagName: 'link',
+      attributes: {
+        rel: 'preload',
+        href: '//fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap',
+        as: 'style',
+        onload: "this.onload=null;this.rel='stylesheet'"
+      },
+    },
+    {
+      tagName: 'noscript',
+      attributes: {},
+      innerHTML: `<link rel="stylesheet" href="//fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap">`,
+    },
   ],
 
   stylesheets: [
@@ -67,10 +81,10 @@ const config: Config = {
     //   type: 'text/css',
     // },
 
-    {
-      href: '//fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap',
-      type: 'text/css',
-    },
+    // {
+    //   href: '//fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,100..800;1,100..800&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap',
+    //   type: 'text/css',
+    // },
   ],
   plugins: [
     'docusaurus-plugin-sass',
